465fc406db1af9137d5c2da41c38cb21cc769af5
KrasovskihIA <75630213+KrasovskihIA@users.noreply.github.com>
2021-11-24 00:23:06 +0400

    Correction of errors.
    Correction of errors.
    When creating a task, it was sent to another page. Fixed

--- ListApp/todoapp/urls.py
+++ ListApp/todoapp/urls.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 from django.urls import path
 from . import views 
+from django.conf.urls import url
 
 
 urlpatterns = [
-    path('', views.todoappView, name='index'),
     path('addTodoItem/', views.addToDoView),
     path('deleteTodoItem/<int:i>/', views.deleteTodoView),
+    url(r'^$', views.todoappView, name='index'),
 ]
--- ListApp/todoapp/views.py
+++ ListApp/todoapp/views.py
@@ -12,10 +12,10 @@ def addToDoView(request):
     content_text = request.POST['content']
     new_item = ToDoList(content=content_text)
     new_item.save()
-    return HttpResponseRedirect('')
+    return HttpResponseRedirect('/')
 
 #Удаление задачи
 def deleteTodoView(request, i):
     deleteapp = ToDoList.objects.get(id= i)
     deleteapp.delete()
-    return HttpResponseRedirect('')
+    return HttpResponseRedirect('/')
